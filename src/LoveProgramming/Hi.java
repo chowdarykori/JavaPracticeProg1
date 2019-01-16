ff988fbaf72bb72f7228664393ba72a8edfc5e1a
unknown <skoripalli@gmail.com>
2019-01-16 00:26:24 +0000

    commit files

---
+++ src/LoveProgramming/Hi.java
@@ -0,0 +1,8 @@
+package LoveProgramming;
+
+public class Hi {
+    public static void main(String[] args) { // main function
+        System.out.print( "\'hello world\'");
+        System.out.println("Hello\t\t\t World"); // note this three taps
+    }
+}
